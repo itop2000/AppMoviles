e8438149e42989ce1742b7e95b70c39a634a234a
Kuro <muges453012@gmail.com>
2024-10-16 20:42:28 -0600

    Manejo de la consola

---
+++ Clase_161024/script.js
@@ -0,0 +1,11 @@
+//console.log("hi");
+
+//manejo de consola en un html
+//documet.title
+//clear()
+//
+
+const h1 = document.createElement("h1");
+h1.textContent = "hola clase DOM desde js";
+//console.log(h1.textContent);
+document.body.appendChild(h1);
